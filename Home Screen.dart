8bef1437e40f8e4876d6803441d2968649d58e99
Abdi Ali <160644671+Ibrqh@users.noreply.github.com>
2024-05-20 12:46:22 +0300

    Add files via upload

---
+++ Home Screen.dart
@@ -0,0 +1,20 @@
+class HomeScreen extends StatelessWidget {
+  final List<String> items = ['Item 1', 'Item 2', 'Item 3'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ Splash Screen.dart
@@ -0,0 +1,25 @@
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:
+            Image.asset('assets/logo.png'), // Replace with your logo image path
+      ),
+    );
+  }
+}
